86a37097742781cd4c3dafac75483e8af678360d
Your Name <you@example.com>
2020-02-08 16:21:53 +0530

    upgraded some funtionality like right answer wrong answer and many more

--- app/src/main/java/com/smartherd/quizapp/QuizActivity.kt
+++ app/src/main/java/com/smartherd/quizapp/QuizActivity.kt
@@ -1,7 +1,10 @@
 package com.smartherd.quizapp
 
+import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_quiz.*
 import kotlin.random.Random
@@ -20,8 +23,23 @@ class QuizActivity : AppCompatActivity() {
 
         startQuiz()
     }
+    fun rightAnswer(){
+        right_answer.visibility = View.VISIBLE
+        right_answer.setBackgroundColor(Color.GREEN)
+        right_answer.text = question
+
+        val handler = Handler()
+        handler.postDelayed({startQuiz()},3000)
+    }
+    fun wrongAnswer(){
+        right_answer.visibility = View.VISIBLE
+        right_answer.setBackgroundColor(Color.RED)
+        right_answer.text = user_answer.text
+    }
 
     fun startQuiz() {
+        right_answer.text = ""
+        right_answer.visibility = View.INVISIBLE
         random = Random
        question = questions[random.nextInt(questions.size)]
         question_textview.text = mixLetter(question)
@@ -30,14 +48,24 @@ class QuizActivity : AppCompatActivity() {
     }
 
     fun showAnswer(view: View) {
-
+        right_answer.visibility = View.VISIBLE
+        right_answer.text = question
     }
 
     fun checkAnswer(view: View) {
 
+        if (user_answer.text.toString().equals(question, ignoreCase = true)){
+            rightAnswer()
+            Toast.makeText(this, "good job", Toast.LENGTH_SHORT).show()
+        }else{
+            wrongAnswer()
+            Toast.makeText(this, "Bad job", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     fun changeAnswer(view: View) {
+        startQuiz()
 
     }
     fun mixLetter (word:String):String{
